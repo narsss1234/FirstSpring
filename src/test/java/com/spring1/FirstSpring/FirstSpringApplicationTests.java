0381b30304d02d462b8cea2a63a68956b38fbf43
narsss1234 <narsss1234@gmail.com>
2026-01-02 16:18:24 +0530

    used spring initializer for basic folder structure

---
+++ src/test/java/com/spring1/FirstSpring/FirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring1.FirstSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
